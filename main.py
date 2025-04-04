9f2d3acb45658f67d8da2fc548c8472e5c00e634
Simon Vargas <sjvargas01@gmail.com>
2025-04-03 23:12:42 -0400

    added new label

---
+++ main.py
@@ -0,0 +1,8 @@
+import FreeSimpleGUI as sg
+
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input(size=(25, 1), key='-IN-')
+choose_button = sg.FilesBrowse("Browse", key='-BROWSE-')
+
+label2 = sg.Text("Select destination folder:")
+input2 = sg.Input()
