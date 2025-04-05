f7b7b66e6bfe9826e1cab85d153c1f29d9e859c9
Simon Vargas <sjvargas01@gmail.com>
2025-04-05 11:56:15 -0400

    added a compress button in window

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose")
 
+compress_button = sg.Button("Compress", key='-COMPRESS-')
+
 window = sg.Window("File Compressor", 
                    layout=[[label1, input1, choose_button],
-                           [label2, input2, choose_button2]])
+                           [label2, input2, choose_button2],
+                           [compress_button]],)
 
 
 window.read()
